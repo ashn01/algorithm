261853ffc7716a6adca0fdb396ccb7cb6b7ac6d8
YMK <midvalue@gmail.com>
2018-01-25 03:40:02 -0500

    al1

---
+++ 760. Find Anagram Mappings.cpp
@@ -0,0 +1,95 @@
+/* https://leetcode.com/problems/find-anagram-mappings/description/#
+ * Given two lists Aand B, and B is an anagram of A. B is an anagram of A means B is made by randomizing the order of the elements in A.
+ *
+ * We want to find an index mapping P, from A to B. A mapping P[i] = j means the ith element in A appears in B at index j.
+ *
+ * These lists A and B may contain duplicates. If there are multiple answers, output any of them.
+ *
+ * For example, given
+ *
+ * A = [12, 28, 46, 32, 50]
+ * B = [50, 12, 32, 46, 28]
+ * We should return
+ * [1, 4, 3, 2, 0]
+ * as P[0] = 1 because the 0th element of A appears at B[1], and P[1] = 4 because the 1st element of A appears at B[4], and so on.
+ * Note:
+ * 
+ * A, B have equal lengths in range [1, 100].
+ * A[i], B[i] are integers in range [0, 10^5].
+ */
+
+
+class Solution {
+public:
+    vector<int> anagramMappings(vector<int>& A, vector<int>& B) {
+        vector<int> ret;
+        vector<int>::iterator it;
+        
+        for(int i=0;i<A.size();i++)
+        {
+            int val = find(B.begin(),B.end(),A.at(i))-B.begin();
+            ret.push_back(val);
+        }
+        
+        return ret;
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) {
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim)) {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+string integerVectorToString(vector<int> list, int length = -1) {
+    if (length == -1) {
+        length = list.size();
+    }
+
+    if (length == 0) {
+        return "[]";
+    }
+
+    string result;
+    for(int index = 0; index < length; index++) {
+        int number = list[index];
+        result += to_string(number) + ", ";
+    }
+    return "[" + result.substr(0, result.length() - 2) + "]";
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        vector<int> A = stringToIntegerVector(line);
+        getline(cin, line);
+        vector<int> B = stringToIntegerVector(line);
+        
+        vector<int> ret = Solution().anagramMappings(A, B);
+
+        string out = integerVectorToString(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
