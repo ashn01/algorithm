fe551d5483719cc7466cc58e593466212b4d7f88
YMK <midvalue@gmail.com>
2018-01-29 23:57:34 -0500

    algorithm

---
+++ CombinationSumIII.cpp
@@ -0,0 +1,64 @@
+//https://leetcode.com/problems/combination-sum-iii/description/
+//Find all possible combinations of k numbers that add up to a number n, given that only numbers from 1 to 9 can be used and each combination should be a unique set of numbers.
+//
+//
+//Example 1:
+//
+//Input: k = 3, n = 7
+//
+//Output :
+//
+//[[1, 2, 4]]
+//
+//Example 2 :
+//
+//Input : k = 3, n = 9
+//
+//Output :
+//
+//[[1, 2, 6], [1, 3, 5], [2, 3, 4]]
+
+class Solution {
+public:
+	vector<vector<int>> combinationSum3(int k, int n) {
+		vector<vector<int>> ret;
+		vector<int> vt;
+		engine(ret, vt, 0, k, n);
+		return ret;
+	}
+	int engine(vector<vector<int>>& ret, vector<int> vt, const int cur, const int k, const int n)
+	{
+		int temp = 0;
+		for (auto& a : vt) // if sum exceeds, terminate current routine.
+		{
+			temp += a;
+			if (temp > n)
+				return 1;
+		}
+		for (int i = cur + 1; i <= 9; i++)
+		{
+			if (vt.size() < k)
+			{
+				vt.push_back(i);
+				if (vt.size() == k)
+				{
+					int sum = 0;
+					for (auto& a : vt)
+						sum += a;
+					if (sum == n)
+					{
+						ret.push_back(vt);
+						return 1;
+					}
+					vt.pop_back();
+				}
+				else
+				{
+					engine(ret, vt, i, k, n);
+					vt.pop_back();
+				}
+			}
+		}
+		return 1;
+	}
+};
